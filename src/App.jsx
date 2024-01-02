f950ee0dbb240d96b246e8b13a99228523da7d02
Timileyin Akintilo <96595056+Timmtet@users.noreply.github.com>
2024-01-02 18:37:30 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Timileyin Akintilo",
+  title: "Data Scientist",
+  email: "akintilotimileyin@gmail.com",
+  gitHub: "https://github.com/Timmtet",
+  instagram: "https://www.instagram.com/_timmtet/",
+  linkedIn: "https://www.linkedin.com/in/timileyin-akintilo-193192188/",
+  medium: "https://medium.com/@akintilotimileyin",
+  twitter: "https://twitter.com/Timmtet",
+  youTube: "https://www.youtube.com/@akintilotimileyin6202",
 };
 
 const primaryColor = "#4E567E";
