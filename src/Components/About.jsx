68b9c75e915199711355da7b357ddf4e5e8e2060
Timileyin Akintilo <96595056+Timmtet@users.noreply.github.com>
2024-01-02 19:49:18 +0000

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "A data scientist with 3 years of experience, proffering data-driven solutions.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "",
+  "Python",
+  "Mysql",
+  "Power Bi",
+  "R",
+  "Git",
 ];
 
 /**
@@ -45,7 +45,10 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "A data scientist with 3 years of experience, bringing expertise in data gathering, cleaning, and analysis to extract
+valuable insights. Proficient in applying statistical, algebraic, and various analytical techniques to generate
+meaningful insights from data. Possesses a high level of organization, motivation, and diligence, with a significant
+background in data analysis. Committed to delivering impactful results through data-driven approaches";
 
 const About = () => {
   return (
